c734e6d25cc7b16224051d3aaebfb2d9a263aaf6
Fabricio Bernardes <fbernardes.dev@gmail.com>
2024-09-22 16:24:39 -0300

    fix: configure cors

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,25 @@
-
 import express, { Application } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './src/routes';
 const app: Application = express();
 
+const allowedOrigins = ['http://127.0.0.1:8001', 'http://localhost:8001'];
 const port = process.env.PORT || 8000;
 
 dotenv.config();
 app.use(express.json());
 
 app.use(cors({
-        origin: 'http://127.0.0.1:8001',
-        credentials: true
-    }));
+    origin: (origin, callback) => {
+        if (origin && allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 app.use(express.urlencoded({ extended: true }));
 
